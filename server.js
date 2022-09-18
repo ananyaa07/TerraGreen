64c316fde19d9dc1e600160cc41d6a1c4d5f9144
Ananya <goyalananya444@gmail.com>
2022-09-18 21:17:15 +0530

    changes done

--- server.js
+++ server.js
@@ -42,8 +42,8 @@ app.post("/", (req, res) => {
         subject: "Join the TerraGreen Community",
         html: `We are a community of people committed to improving the world that we live in. We aim to spread awareness about the various climate change phenomena and their causes, and encourage the public to take steps like recycling, minimise wastage and dedicate their time for a good cause by participating in environmental campaigns. To get notified about future campaigns, follow us on Instagram. <br> <br> <img src = "cid:unique@kreata.ee"/>`,
         attachments: [{
-                filename: 'POSTER.png',
-                path: 'public/images/POSTER.png',
+                filename: 'POSTER2.jpeg',
+                path: 'public/images/POSTER2.jpeg',
                 cid: 'unique@kreata.ee',
               }]
     }
